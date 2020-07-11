ed3148db9e95ce9c24797e0af560e3c275ded5b9
TohurTV <joshwebb84@outlook.com>
2020-07-11 02:57:41 -0600

    Adding Discord Support and made  alot of improvements

--- classes/DiscordBotClass.php
+++ classes/DiscordBotClass.php
@@ -33,7 +33,7 @@ class DiscordBotClass
             echo 'Received Message from ' . $message->author->tag . ' in ' . ($message->channel instanceof \CharlotteDunois\Yasmin\Interfaces\DMChannelInterface ? 'DM' : 'channel #' . $message->channel->name) . ' with ' . $message->attachments->count() . ' attachment(s) and ' . \count($message->embeds) . ' embed(s)' . PHP_EOL;
         });
 
-        $client->login('')->done();
+        $client->login($config['token'])->done();
         $loop->run();
     }
 }
